5817f0087982609a8ad91c4f6fa2f8b9e7fe5da5
Ben McGuirk <benmcguirk15@gmail.com>
2024-01-17 14:09:47 +0000

    py

--- YT_Flask/app.py
+++ YT_Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -22,9 +22,48 @@ class Todo(db.Model):
 def make_shell_context():
     return {'app': app, 'db': db, 'Todo': Todo}
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content'] # get the content from the form
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task) # add the new task to the database
+            db.session.commit() # commit the new task to the database
+            return redirect('/') # redirect to the index page
+        except:
+            return 'There was an issue adding your task'
+    else:  
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete) # delete the task
+        db.session.commit() # commit the deletion
+        return redirect('/') # redirect to the index page
+    except:
+        return 'There was a problem deleting that task'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+
+        try:
+            db.session.commit() # commit the update
+            return redirect('/') # redirect to the index page
+        except:
+            return 'There was an issue updating your task'
+        
+    else: 
+        return render_template('update.html', task=task_to_update or None)
 
 if __name__ == '__main__':
     app.run(debug=True)
