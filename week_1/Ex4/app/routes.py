ddb3559c42a22d23fa300b3c02e479c3a64e6313
Ben McGuirk <benmcguirk15@gmail.com>
2024-01-17 15:23:13 +0000

    Ex4

--- week_1/Ex4/app/routes.py
+++ week_1/Ex4/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask import Blueprint
 
 main_bp = Blueprint('main', __name__)
@@ -7,10 +7,25 @@ main_bp = Blueprint('main', __name__)
 def main():
     return render_template('index.html')
 
-@main_bp.route('/app')
-def app():
-    return render_template('app.html')
+@main_bp.route('/app') #return values of App object
+def app_info():
+    from app import create_app
+    app = create_app()
+    info = {
+        'url_map': str(app.url_map),
+    }
+    return render_template('app.html', app_info=jsonify(info).json)
 
 @main_bp.route('/request')
-def request():
-    return render_template('request.html')
+def request_info():
+    info = {
+        'args': str(request.args),
+        'cookies': str(request.cookies),
+        'headers': str(request.headers),
+        'method': str(request.method),
+        'url': str(request.url),
+        'url_root': str(request.url_root),
+        'user_agent': str(request.user_agent),
+    }
+
+    return render_template('request.html', request_info=jsonify(info).json)
