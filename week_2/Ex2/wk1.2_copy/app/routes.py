db19def3d005c643553c99a35257d4330368e257
Ben McGuirk <benmcguirk15@gmail.com>
2024-02-12 11:43:57 +0000

    Update page title and add random quote functionality

--- week_2/Ex2/wk1.2_copy/app/routes.py
+++ week_2/Ex2/wk1.2_copy/app/routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+import random
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-def main():
-    return render_template('index.html')
+def quote():
+    with open('quotes.txt') as file:
+        quotes = file.readlines()
+    theQuote = random.choice(quotes)
+
+    return render_template('index.html', quote=jsonify(theQuote).json)
