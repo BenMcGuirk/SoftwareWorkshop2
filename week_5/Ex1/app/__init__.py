25e27ee7c6e49353118d6b3ed099fc45bb6f6e92
Ben McGuirk <benmcguirk15@gmail.com>
2024-02-19 12:03:14 +0000

    sqlalchemy

--- week_5/Ex1/app/__init__.py
+++ week_5/Ex1/app/__init__.py
@@ -4,16 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'487564sfdljgsfkgjw4o0'
+app.config['SECRET_KEY'] =  b'WR#&f&+%78er0we=%799eww+#7^90-;s'
 
-# Database
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data', 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from app.views import *
-from app.models import Student, Loan
+
+from app import views
+from app.models import *
 
 @app.shell_context_processor
 def make_shell_context():
--- week_5/Ex1/app/models.py
+++ week_5/Ex1/app/models.py
@@ -1,7 +1,8 @@
 from app import db
+
 class Student(db.Model):
     __tablename__ = 'students'
-    student_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False, index=True)
+    student_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True, index=True)
     firstname = db.Column(db.String(32))
     lastname = db.Column(db.String(32), nullable=False, index=True)
@@ -9,16 +10,17 @@ class Student(db.Model):
     loans = db.relationship('Loan', backref='student', lazy='dynamic')
 
     def __repr__(self):
-        return f"student('{self.username}', '{self.firstname}', '{self.lastname}', '{self.email}')"
-    
+        return f"student('{self.username}', '{self.lastname}', '{self.firstname}' , '{self.email}')"
+
 class Loan(db.Model):
     __tablename__ = 'loans'
-    loan_id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False, index=True)
+    loan_id = db.Column(db.Integer, primary_key=True)
     device_id = db.Column(db.Integer, nullable=False)
     borrowdatetime = db.Column(db.DateTime, nullable=False)
     returndatetime = db.Column(db.DateTime, nullable=True)
     student_id = db.Column(db.Integer, db.ForeignKey('students.student_id'), nullable=False)
 
     def __repr__(self):
-        return f"loan('{self.device_id}', '{self.borrowdatetime}', '{self.returndatetime}', '{self.student_id}')"
-    
+        return f"loan('{self.device_id}', '{self.borrowdatetime}' , '{self.returndatetime}', '{self.student}')"
+
+
